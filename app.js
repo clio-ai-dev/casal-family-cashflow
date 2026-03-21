26cd32018f9415330cdba79a479c289187627064
Ubuntu <azureuser@openclaw-vm.4sycooya3ajezi10xccj24cznf.phxx.internal.cloudapp.net>
2026-03-21 17:26:44 +0000

    Remove custom legend, use compact Chart.js legend below chart

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ function renderSourceChart(data) {
     options: {
       responsive: true,
       plugins: {
-        legend: { display: false },
+        legend: { position: 'bottom', labels: { color: '#aaa', usePointStyle: true, pointStyle: 'circle', padding: 8, font: { size: 11 }, boxWidth: 8 } },
         tooltip: {
           callbacks: {
             label: ctx => `${ctx.dataset.label}: $${ctx.raw.toLocaleString()}`
